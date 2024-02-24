86d0e64826e3dd43b7fc97853a6cd0a6bcbcd2f5
welliton gervickas <wellitogervickas@gmail.com>
2024-02-24 19:11:42 -0300

    finish server

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::{
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -24,31 +24,61 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        let job: Box<F> = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap()
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread: thread::JoinHandle<()> = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing...", id);
-            job();
+            let message: Result<Box<dyn FnOnce() + Send>, mpsc::RecvError> =
+                receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id}^disconnected; shutting down");
+                    break;
+                }
+            }
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,32 +12,42 @@ fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream: TcpStream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down");
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let bug_reader: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
-    let request_line: String = bug_reader.lines().next().unwrap().unwrap();
+    let mut buffer: [u8; 1024] = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    let get: &[u8; 16] = b"GET / HTTP/1.1\r\n";
+    let sleep: &[u8; 21] = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let contents: String = fs::read_to_string(filename).unwrap();
-    let length: usize = contents.len();
 
-    let response: String = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response: String = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
 
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
